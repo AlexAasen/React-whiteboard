46ae7b2fff1af1f1f43c44d4e9d28b2563ab822f
jonnakollin <joonnaaa_@hotmail.com>
2016-10-12 21:26:51 +0200

    second comit cuz i was clumsy

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,73 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var cleanCSS = require('gulp-clean-css');
-var browserSync = require('browser-sync').create();
-var htmlmin = require('gulp-htmlmin');
+/* eslint import/no-extraneous-dependencies: ["off", {"devDependencies": false}] */
 
-gulp.task('browserSync', function() {
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
+const htmlmin = require('gulp-htmlmin');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', () =>
   browserSync.init({
     server: {
-      baseDir: 'public'
-    },
+      baseDir: 'dist'
+    }
   })
-});
+);
 
-gulp.task('css', function() {
-  return gulp.src(['public/css/reset.css', 'public/css/global.css'])
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer())
-    .pipe(concat('all.css'))
-    .pipe(cleanCSS())
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('public/dist/css'))
-    .pipe(browserSync.reload({
-      stream: true
-    }));
-});
+gulp.task('css', () =>
+  gulp.src(['./public/css/reset.css', './public/css/global.css'])
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer())
+  .pipe(concat('master.css'))
+  .pipe(cleanCSS())
+  .pipe(sourcemaps.write('.'))
+  .pipe(gulp.dest('./dist/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+);
 
-gulp.task('javascript', function(){
-  return gulp.src(['node_modules/jquery/dist/jquery.js', 'public/js/client.js'])
-          .pipe(sourcemaps.init())
-          .pipe(concat('main.js'))
-          .pipe(uglify())
-          .pipe(sourcemaps.write('.'))
-          .pipe(gulp.dest('public/dist/js'))
-          .pipe(browserSync.reload({
-            stream: true
-          }));
-});
+gulp.task('html', () => gulp.src('./public/**/*.html')
+  .pipe(htmlmin({
+    collapseWhitespace: true
+  }))
+  .pipe(gulp.dest('./dist'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+);
 
-gulp.task('html', function() {
-  return browserSync.reload();
-});
+gulp.task('images', () => gulp.src('./public/img/**.*')
+  .pipe(gulp.dest('./dist/img'))
+);
 
-gulp.task('default', ['browserSync', 'css' , 'javascript' ], function() {
-  gulp.watch('public/css/*.css', ['css']);
-  gulp.watch('public/js/*.js', ['javascript']);
-  gulp.watch('public/*.html', ['html']);
+gulp.task('javascript', () => browserify('./public/js/app.js', {
+  debug: true
+})
+  .transform(babelify)
+  .bundle()
+  .pipe(source('whiteboard.bundle.js'))
+  .pipe(buffer())
+  .pipe(sourcemaps.init({
+    loadMaps: true
+  }))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./dist/js'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
+);
+
+gulp.task('default', ['browserSync', 'html', 'css', 'javascript', 'images'], () => {
+  gulp.watch('./public/css/**/*.css', ['css']);
+  gulp.watch('./public/src/img/**.*', ['images']);
+  gulp.watch('./public/js/**/*.js*', ['javascript']);
+  gulp.watch('./public/**/*.html', ['html']);
 });
